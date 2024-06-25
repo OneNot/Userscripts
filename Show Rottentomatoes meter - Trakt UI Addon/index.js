39472a6fe32309f73aa72874856a66a1144cd1f6
Toni Hannula <toni.hannula@xamk.fi>
2024-06-25 07:40:44 +0300

    Added WIP of Show Rottentomatoes meter - Trakt UI Addon

---
+++ Show Rottentomatoes meter - Trakt UI Addon/index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Show Rottentomatoes meter - Trakt UI Addon
+// @description This is an addon script for "Show Rottentomatoes meter" by cuzi, that displays the Rottentomatoes info cleanly on the trakt.tv infobar
+// @namespace   1N07
+// @author      1N07
+// @license     MIT
+// @icon        https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/color/72x72/1F345.png
+// @version     48.1.0
+// @match       https://trakt.tv/movies/*
+// @match       https://trakt.tv/shows/*
+// @run-at      document-body
+// ==/UserScript==
+
+(async function () {
+  
+})()
