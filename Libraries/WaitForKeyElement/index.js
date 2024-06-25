62df797de022dd8ac1843e1282e1c3767f70a410
Toni Hannula <toni.hannula@xamk.fi>
2024-06-25 07:53:44 +0300

    WaitForKeyElement: Added missing selector

--- Libraries/WaitForKeyElement/index.js
+++ Libraries/WaitForKeyElement/index.js
@@ -10,7 +10,7 @@
 
 async function WaitForKeyElement(selector, timeout) {
     return new Promise((resolve, reject) => {
-      let el = document.querySelector();
+      let el = document.querySelector(selector);
 
       if(el) {
         resolve(el);
