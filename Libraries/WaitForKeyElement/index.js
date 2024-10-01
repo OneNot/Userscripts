9ec8dd2d276fd7beded05b6874ed12054dc29e28
Toni Hannula <toni.hannula@xamk.fi>
2024-10-01 14:56:26 +0300

    WaitForKeyElement: 1.0.1 - Reject reason

--- Libraries/WaitForKeyElement/index.js
+++ Libraries/WaitForKeyElement/index.js
@@ -3,7 +3,7 @@
 // @license     MIT
 // @namespace   1N07
 // @match       *://*/*
-// @version     1.0
+// @version     1.0.1
 // @author      1N07
 // @description Library that Exports my simplified vanilla JS version of WaitForKeyElement, which is a simple async function that returns a Promise that resolves to an element by a given selector, when that element is found
 // ==/UserScript==
@@ -11,12 +11,10 @@
 async function WaitForKeyElement(selector, timeout) {
 	return new Promise((resolve, reject) => {
 		let el = document.querySelector(selector);
-
 		if (el) {
 			resolve(el);
 			return;
 		}
-
 		new MutationObserver((mutationRecords, observer) => {
 			el = document.querySelector(selector);
 			if (el) {
@@ -24,10 +22,9 @@ async function WaitForKeyElement(selector, timeout) {
 				observer.disconnect();
 			}
 		}).observe(document.documentElement, { childList: true, subtree: true });
-
 		if (timeout) {
 			setTimeout(() => {
-				reject(null);
+				reject(`WaitForKeyElement: "${selector}" timed out`);
 			}, timeout);
 		}
 	});
